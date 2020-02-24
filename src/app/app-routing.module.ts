48491805e7824cace5789f45f324c042ecdc6d9d
Justin Blobaum <justinblobaum@Justins-MacBook-Air.local>
2020-02-24 16:55:18 -0600

    rename search and add cocktail link to header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { LogInComponent } from './log-in/log-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserComponent } from './user/user.component';
 import { SearchComponent } from './search/search.component';
+import { CocktailComponent } from './cocktail/cocktail.component';
 
 
 const routes: Routes = [
   {path: 'login', component: LogInComponent},
   {path: 'signup', component: SignUpComponent},
   {path: 'user', component: UserComponent},
-  {path: 'search', component:SearchComponent},
+  {path: 'entree', component:SearchComponent},
+  {path: 'cocktail', component: CocktailComponent}
   {path: '**', component:LogInComponent}
 ];
 
