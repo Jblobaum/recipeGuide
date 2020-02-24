2930418846829f22f068a453b5d7d7dee900e8b4
Justin Blobaum <justinblobaum@Justins-MacBook-Air.local>
2020-02-24 15:34:35 -0600

    Added basic sign up form

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CrossFieldMatcher } from '../validators/matcher';
+import { passwordMatchValidator } from '../validators/confirmPassword';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
+  loginForm: FormGroup;
+  matcher: CrossFieldMatcher;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    ) {
+      this.matcher = new CrossFieldMatcher()
+    }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      user: ['', Validators.compose([Validators.required, Validators.maxLength(16), Validators.minLength(3)])],
+      pass: ['', Validators.compose([Validators.required, Validators.maxLength(128), Validators.minLength(8)])],
+      confirmPassword: ['' ]
+    }, {validator: passwordMatchValidator});
   }
 
 }
