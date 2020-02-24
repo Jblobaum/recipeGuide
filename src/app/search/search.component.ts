e04b1175cf9694dcab961022a01dc5624d6a9b7e
Justin Blobaum <justinblobaum@Justins-MacBook-Air.local>
2020-02-24 16:34:52 -0600

    cleaned up search branch

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { fromEvent } from 'rxjs';
 import { map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
