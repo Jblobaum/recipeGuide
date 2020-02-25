e9802fcd56154471dd4cb80d8cae6fc7c7122a98
Justin Blobaum <justinblobaum@Justins-MacBook-Air.local>
2020-02-24 22:35:26 -0600

    bug fixing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LogInComponent } from './log-in/log-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserComponent } from './user/user.component';
-import { SearchComponent } from './search/search.component';
-import { CocktailComponent } from './cocktail/cocktail.component';
+import { SearchComponent } from './cocktail/cocktail.component';
 
 
 const routes: Routes = [
   {path: 'login', component: LogInComponent},
   {path: 'signup', component: SignUpComponent},
   {path: 'user', component: UserComponent},
-  {path: 'entree', component:SearchComponent},
+  {path: 'entree', component: SearchComponent},
   {path: 'cocktail', component: CocktailComponent},
   {path: '**', component:LogInComponent}
 ];
