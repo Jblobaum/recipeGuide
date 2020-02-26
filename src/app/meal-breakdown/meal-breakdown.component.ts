d54254dc470b4631ebbb783632b0d3bb887cd098
Justin Blobaum <justinblobaum@Justins-MacBook-Air.local>
2020-02-25 18:57:11 -0600

    added layout and started functionality

--- src/app/meal-breakdown/meal-breakdown.component.ts
+++ src/app/meal-breakdown/meal-breakdown.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./meal-breakdown.component.scss']
 })
 export class MealBreakdownComponent implements OnInit {
+  drink: Object;
+  ingredients: Array<Object>;
 
-  constructor() { }
+  constructor(
+    private drinkByIDService: DrinkByIdService,
+    private actr: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
-  }
+    this.drinkByIDService.drinksById(this.actr.snapshot.params.id).subscribe(results => {
+      this.drink = results['drinks'][0]
+      this.ingredients = []
+      let measurements = []
+    
+      Object.keys(results['drinks'][0]).map(item => {
+        if(item.match("strIngredient") && this.drink[item] != null){
+         measurements =  item.split("strIngredient")
+          console.log(measurements);
+          let key = `strMeasure${measurements[1]}`
+          console.log(this.drink[key]);
+          
+       this.ingredients.push({ingredient: this.drink[item], measurement: this.drink[key]})
 
-}
+      }
+       
+       
+      })
+      
+      console.log(this.ingredients);
+      
+    });
+  }
+    
+
+
