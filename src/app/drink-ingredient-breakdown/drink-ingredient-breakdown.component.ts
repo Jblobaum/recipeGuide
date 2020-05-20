4b619b03ca17e24eda9b081155139e7cbfee0999
Jblobaum <justin.blobaum@midlandu.edu>
2020-05-20 11:27:19 -0500

    updated drink ingredient breakdown

--- src/app/drink-ingredient-breakdown/drink-ingredient-breakdown.component.ts
+++ src/app/drink-ingredient-breakdown/drink-ingredient-breakdown.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import { RootState } from '../store';
 import { DrinkByIngredientService } from '../services/drink-by-ingredient.service';
 import * as Actions from '../store/actions'
+import * as Selectors from '../store/selectors'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-drink-ingredient-breakdown',
@@ -16,6 +18,8 @@ export class DrinkIngredientBreakdownComponent implements OnInit {
   recipes: Array<Object>;
   categories: Array<Object>;
   ingredient: string
+  breakpoint: number;
+  user$: Observable<Object>
 
   constructor(
     private actr: ActivatedRoute,
@@ -24,10 +28,12 @@ export class DrinkIngredientBreakdownComponent implements OnInit {
     private drinkByIngredient: DrinkByIngredientService
   ) { 
     this.ingredient = this.actr.snapshot.params.ingredient
+    this.user$ = store.pipe(select(Selectors.getUserState))
+  this.user$.subscribe(val=> console.log(val));
   }
 
-  addFavMeal(recipeToAdd) {
-    this.store.dispatch(Actions.addRecipe({ recipe: recipeToAdd }))
+  addFavDrink(recipe) {
+    this.store.dispatch(Actions.addRecipe({ recipe: recipe }))
 
   }
 
@@ -39,10 +45,15 @@ export class DrinkIngredientBreakdownComponent implements OnInit {
     this._snackBar.open(this.message, null, config);
   };
 
+  onResize(event) {
+    this.breakpoint = (event.target.innerWidth <= 650) ? 2 : 5;
+  }
+
   ngOnInit(): void {
     this.drinkByIngredient.drinkById(this.actr.snapshot.params.ingredient).subscribe(results => {
       this.recipes = results['drinks']
     })
+    this.breakpoint = (window.innerWidth <= 650) ? 2 : 5;
   }
 
 }
